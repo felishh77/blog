84746213cf7ee8cdaeca23fd7ddc1ac9ddd848b2
felix07 <l.shuheng@stu.xjtu.edu.cn>
2025-11-22 17:40:11 +0800

    feat: update textScrambble

--- src/components/TextScramble.svelte
+++ src/components/TextScramble.svelte
@@ -56,15 +56,18 @@ class TextScramble {
 }
 
 const phrases = [
-	"Hi!",
-	"Welcome to this blog!",
-	"I'm Felix,",
-	"A drifter in the world",
-	"just like you.",
-	"This is not only a site,",
-	"it's also a campsite.'",
-	"It brings warmth to me",
-	"and hopefully to you."
+	"Hello?",
+	"Is there anybody in there?",
+	"Just nod if you can hear me",
+	"Is there anyone home?",
+	"Come on, now",
+	"I hear you're feeling down.",
+	"Well I can ease your pain,",
+	"Get you on your feet again.",
+	"fqwefeqwgrgw",
+	"dwdewfrrg",
+	"poktr",
+	"Hello."
 ];
 
 let el;
@@ -79,9 +82,12 @@ onMount(() => {
 		const phrase = phrases[counter];
 		fx.setText(phrase).then(() => {
 			// 根据 phrase 长度计算延迟：每个字符 80ms，最小 500ms，最大 3000ms
-			let delay = Math.min(Math.max(phrase.length * 80, 500), 3000);
-			if (phrase === "and hopefully to you.") {
-				delay = 5000; // 例如 5 秒
+			let delay = Math.min(Math.max(phrase.length * 100, 800), 4000);
+			if (phrase === "Is there anybody home?" || phrase === "Hello.") {
+				delay = 7500; // 例如 5 秒
+			}
+			if (phrase === "fqwefeqwgrgw" || phrase === "dwdewfrrg" || phrase === "poktr") {
+				delay = 300; // 例如 5 秒
 			}
 			setTimeout(next, delay);
 		});
